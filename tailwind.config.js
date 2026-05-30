6e1d2d985554260aa5db0c21cb998a74177072eb
dlucioyauh <dlucio.douglas@gmail.com>
2026-05-30 18:47:46 -0300

    fix: adiciona config do Tailwind com darkMode class

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: 'class',  // ← ESSENCIAL para alternar entre temas
+  darkMode: 'class',
   theme: {
     extend: {},
   },
